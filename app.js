195671939cad5aa64c7f313a19f45d4479f8b5c1
Przemysław Sz <you@example.com>
2021-11-13 15:02:57 +0100

    Add new port on heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { todoRouter } = require('./routes/todo');
 
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/todo', todoRouter);
 
-app.listen(3000, '0.0.0.0');
+app.listen(port, '0.0.0.0');
